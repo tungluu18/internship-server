278c3317e20737b44d61063a5e8f1751329d4d1a
tungluu18 <tungluu18@gmail.com>
2018-05-16 14:57:12 +0700

    thêm ngày cho ảnh upload

--- model/storage.js
+++ model/storage.js
@@ -9,7 +9,7 @@ module.exports = {
      storage: multer.diskStorage({
        destination: './data/avatar',
        filename: (req, file, cb) => {
-         cb(null, file.fieldname + path.extname(file.originalname))
+         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
        }
      }),
 
